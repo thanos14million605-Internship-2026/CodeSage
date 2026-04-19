583471a7d514f1b21b0e8b3259459c59d36c1ff0
thanos14million605 <gajaga.dev@example.com>
2026-04-19 13:01:39 +0530

    Updated origins for cors

--- backend/fastapi/app/main.py
+++ backend/fastapi/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5000"],
+    allow_origins=["https://codesage-h1pb.onrender.com", "http://localhost:5000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
